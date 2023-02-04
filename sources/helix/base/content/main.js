10a079321eba8608c0b35cc47888f2b61685f65a
TrickyPR <23250792+trickypr@users.noreply.github.com>
2023-02-04 20:15:53 +1100

    Add a dev menu

--- sources/helix/base/content/main.js
+++ sources/helix/base/content/main.js
@@ -1,19 +1,7 @@
-import { BrowserToolboxLauncher } from 'resource://devtools/client/framework/browser-toolbox/Launcher.sys.mjs'
+import { addMenus } from './menus.mjs'
 
 function showMore() {
   document.getElementById('more-text').hidden = false
 }
 
-function startDevtools() {
-  BrowserToolboxLauncher.init()
-}
-
-function reload() {
-  window.location.reload()
-}
-
-document
-  .getElementById('start-devtools')
-  .addEventListener('command', startDevtools)
-
-document.getElementById('reload').addEventListener('command', reload)
+addMenus()
---
+++ sources/helix/base/content/menus.mjs
@@ -0,0 +1,97 @@
+// @ts-check
+import { BrowserToolboxLauncher } from 'resource://devtools/client/framework/browser-toolbox/Launcher.sys.mjs'
+
+/**
+ * @typedef {Object} Menu
+ * @property {string} id
+ * @property {string} label
+ * @property {string} shortcut
+ * @property {string} modifiers
+ * @property {string} keyId
+ * @property {() => void} oncommand
+ */
+
+/** @type {Menu[]} */
+const menus = [
+  {
+    id: 'dev_reloadChrome',
+    label: 'Reload',
+    shortcut: 'R',
+    modifiers: 'accel,shift,alt',
+    keyId: 'key_reloadChrome',
+    oncommand: () => window.location.reload(),
+  },
+  {
+    id: 'dev_browserToolbox',
+    label: 'Browser Toolbox',
+    shortcut: 'I',
+    modifiers: 'accel,shift,alt',
+    keyId: 'key_browserToolbox',
+    oncommand: () => BrowserToolboxLauncher.init(),
+  },
+]
+
+export function addMenus() {
+  /** @type {HTMLDivElement} */
+  const devMenuPopup = document.getElementById('menubar-dev-popup')
+
+  const keyset = document.createXULElement('keyset')
+  keyset.setAttribute('id', 'helix-keyset')
+  document.body.appendChild(keyset)
+
+  for (const menu of menus) {
+    createMenuItem(devMenuPopup, menu)
+    createKey(keyset, menu)
+  }
+}
+
+/**
+ * @param {HTMLDivElement} container This is the containing `<menupopup>`. It is just that type does not exist
+ * @param {Menu} menu The menu to create
+ */
+function createMenuItem(container, menu) {
+  const { label, oncommand } = menu
+  /** @type {HTMLDivElement} */
+  const menuItem = document.createXULElement('menuitem')
+
+  menuItem.label = label
+  menuItem.setAttribute('acceltext', genAccelText(menu))
+
+  menuItem.addEventListener('command', oncommand)
+  container.appendChild(menuItem)
+}
+
+/**
+ * Creates a keybind
+ * @param {HTMLDivElement} container
+ * @param {Menu} key Select the key to create
+ */
+function createKey(container, { shortcut, modifiers, keyId, oncommand }) {
+  /** @type {HTMLDivElement} */
+  const key = document.createXULElement('key')
+  key.id = keyId
+
+  key.setAttribute('key', shortcut)
+  if (modifiers) key.setAttribute('modifiers', modifiers)
+
+  key.addEventListener('command', oncommand)
+  container.appendChild(key)
+}
+
+/**
+ * @param {Menu} menu
+ * @returns {string}
+ *
+ * @todo MacOS support
+ */
+const genAccelText = ({ shortcut, modifiers }) =>
+  [
+    modifiers.includes('accel') ? 'Ctrl' : null,
+    modifiers.includes('shift') ? 'Shift' : null,
+    modifiers.includes('alt') ? 'Alt' : null,
+    shortcut,
+  ]
+    .filter(Boolean)
+    .join('+')
+
+const getKeyElementId = (key) => `key_${key}`
